36e197b98e41875004f09692aa0b2041b7b98ff4
ChokJianHong <116249603+ChokJianHong@users.noreply.github.com>
2024-09-16 20:51:27 +0800

    Updated

---
+++ customer_app/lib/pages/ConfirmationRequest.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ConfirmationRequest extends StatelessWidget {
+  const ConfirmationRequest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- customer_app/lib/pages/RequisitionForm.dart
+++ customer_app/lib/pages/RequisitionForm.dart
@@ -1,5 +1,9 @@
 import 'package:customer_app/Assets/components/CategoryButton.dart';
+import 'package:customer_app/Assets/components/Divider.dart';
+import 'package:customer_app/Assets/components/button.dart';
 import 'package:customer_app/core/configs/theme/app_colors.dart';
+import 'package:customer_app/pages/ConfirmationRequest.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class RequisitionForm extends StatefulWidget {
@@ -39,9 +43,10 @@ class _RequisitionFormState extends State<RequisitionForm> {
               const Text(
                 'CATEGORIES',
                 style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                    color: AppColors.grey),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                  color: AppColors.grey,
+                ),
               ),
               const SizedBox(
                 height: 10,
@@ -61,7 +66,7 @@ class _RequisitionFormState extends State<RequisitionForm> {
                       });
                     },
                   ),
-                  
+
                   // Autogate System Button
                   categoryButton(
                     label: 'AutoGate System',
@@ -75,6 +80,132 @@ class _RequisitionFormState extends State<RequisitionForm> {
                     },
                   ),
                 ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const ADivider(),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "DATE & TIME",
+                style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: AppColors.secondary,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Date:  17 July 2024',
+                        style: TextStyle(fontSize: 12, color: Colors.white),
+                        textAlign: TextAlign.left,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        'Time:  4:00 P.M',
+                        style: TextStyle(fontSize: 12, color: Colors.white),
+                        textAlign: TextAlign.left,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const ADivider(),
+              const Text(
+                'EMERGENCY LEVEL',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('STANDRAD'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('URGENT'),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {}, child: const Text('EMERGENCY')),
+                ],
+              ),
+              const ADivider(),
+              const Text(
+                'PROBLEM DESCRIPTION',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: double.infinity,
+                height: 100,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const ADivider(),
+              const Text(
+                'PICTURE OF PROBLEM',
+                style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              Container(
+                width: double.infinity,
+                color: AppColors.secondary,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                  child: const Text(
+                    'Insert Picture',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              MyButton(
+                text: 'Continue',
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ConfirmationRequest()),
+                  );
+                },
               ),
             ],
           ),
