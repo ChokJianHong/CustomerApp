796e790de6dd66db1f6e4c5649093f81a16fe6da
ChokJianHong <116249603+ChokJianHong@users.noreply.github.com>
2024-09-26 20:30:53 +0800

    Update OrderPage.dart

--- customer_app/lib/pages/OrderPage.dart
+++ customer_app/lib/pages/OrderPage.dart
@@ -1,6 +1,9 @@
 
 import 'package:customer_app/API/GetCustomerOrder.dart';
 import 'package:customer_app/Assets/Model/OrderModel.dart';
+import 'package:customer_app/Assets/components/AppBar.dart';
+import 'package:customer_app/Assets/components/BottomNav.dart';
+import 'package:customer_app/core/configs/theme/app_colors.dart';
 import 'package:customer_app/pages/RequestDetails.dart';
 import 'package:flutter/material.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
@@ -17,6 +20,13 @@ class OrdersPage extends StatefulWidget {
 class _OrdersPageState extends State<OrdersPage> {
   late Future<List<OrderModel>> _ordersFuture;
   late String customerId;
+   int _currentIndex = 2;
+
+  void _onTapTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   void initState() {
@@ -38,89 +48,98 @@ class _OrdersPageState extends State<OrdersPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
-      body: FutureBuilder<List<OrderModel>>(
-        future: _ordersFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text('Error: ${snapshot.error}'),
-            );
-          } else if (snapshot.hasData) {
-            final orders = snapshot.data!;
-            return ListView.builder(
-              itemCount: orders.length,
-              itemBuilder: (context, index) {
-                final order = orders[index];
-                return ListTile(
-                  title: 
-                   Container(
-                    
-                    height: 100,
-
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 20,top: 20),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+      appBar: const CustomAppBar(),
+      backgroundColor: AppColors.primary,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: FutureBuilder<List<OrderModel>>(
+          future: _ordersFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text('Error: ${snapshot.error}'),
+              );
+            } else if (snapshot.hasData) {
+              final orders = snapshot.data!;
+              return ListView.builder(
+                itemCount: orders.length,
+                itemBuilder: (context, index) {
+                  final order = orders[index];
+                  return ListTile(
+                    title: 
+                     Container(
+                      
+                      height: 100,
+        
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20,top: 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Text(order.problemType,style: TextStyle(fontSize: 12),),
-                            SizedBox(height: 20,),
-                            Text(order.orderDate,style: TextStyle(fontSize: 12),)
-                          ],
-                          ),
-                          
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: [
-                                Text('Priority:',style: TextStyle(fontSize: 12),),
-                                SizedBox(width: 5,),
-                                Text(order.urgencyLevel,style: TextStyle(color: Colors.red,fontSize: 12),),
-                              ],
-                            ),
-                            SizedBox(height: 20,),
-                            Row(
-                              children: [
-                                Text('Status:',style: TextStyle(fontSize: 12),),
-                                SizedBox(width: 5,),
-                                Text(order.orderStatus,style: TextStyle(fontSize: 12),)
-                              ],
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(order.problemType,style: TextStyle(fontSize: 12),),
+                              SizedBox(height: 20,),
+                              Text(order.orderDate,style: TextStyle(fontSize: 12),)
+                            ],
                             ),
                             
-                          ],
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Text('Priority:',style: TextStyle(fontSize: 12),),
+                                  SizedBox(width: 5,),
+                                  Text(order.urgencyLevel,style: TextStyle(color: Colors.red,fontSize: 12),),
+                                ],
+                              ),
+                              SizedBox(height: 20,),
+                              Row(
+                                children: [
+                                  Text('Status:',style: TextStyle(fontSize: 12),),
+                                  SizedBox(width: 5,),
+                                  Text(order.orderStatus,style: TextStyle(fontSize: 12),)
+                                ],
+                              ),
+                              
+                            ],
+                          ),
+                          SizedBox(width: 10,)
+                          ], 
                         ),
-                        SizedBox(width: 10,)
-                        ], 
                       ),
                     ),
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>  RequestDetails(
-                          token: widget.token,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>  RequestDetails(
+                            token: widget.token,
+                          ),
                         ),
-                      ),
-                    );
-                  },
-                );
-              },
-            );
-          } else {
-            return const Center(child: Text('No orders available'));
-          }
-        },
+                      );
+                    },
+                  );
+                },
+              );
+            } else {
+              return const Center(child: Text('No orders available'));
+            }
+          },
+        ),
+      ),
+      bottomNavigationBar: BottomNav(
+        onTap: _onTapTapped,
+        currentIndex: _currentIndex,
+        token: widget.token, 
       ),
     );
   }
