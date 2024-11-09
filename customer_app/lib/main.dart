703070cd22176a9d06c838bcb5db74c6bb24536f
WilsonLo99 <loweiseng99@gmail.com>
2024-11-09 16:49:16 +0800

    push notification

--- customer_app/lib/main.dart
+++ customer_app/lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:customer_app/pages/Sign_In.dart';
+import 'API/firebase_api.dart';
 
-void main() => runApp(const MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await FirebaseApi().initNotifications();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
